8d9e687a7f8537108abee0e7c5ca35d1e0d6667d
SSOL <msseock@gmail.com>
2025-08-24 02:36:28 +0900

    feat: 예산 입력받기 서브뷰 구현
    하이파이 버전으로

--- NangManPa/NangManPa/Presentation/InfoInput/SubViews/MoneyInputView.swift
+++ NangManPa/NangManPa/Presentation/InfoInput/SubViews/MoneyInputView.swift
@@ -10,11 +10,56 @@ import SwiftUI
 /// 공사현장의 예산 입력받기 서브뷰
 struct MoneyInputView: View {
     // MARK: Properties
+    /// 입력할 예산
     @Binding var selectedMoneyRange: HowMoneyBig?
+    @State var selectedNumber = 1.0
     
     // MARK: view
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(alignment: .leading, spacing: 0) {
+            Group {
+                Circle()
+                    .frame(width: 20)
+                    .foregroundStyle(Color.keyblue)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.top, 10)
+                    .padding(.bottom, 16)
+                
+                Text("진행중인 건설 현장의\n예산은 얼마인가요?")
+                    .font(NMFont.pre_semibold_22)
+                    .multilineTextAlignment(.leading)
+                
+                Text("슬라이드를 좌우로 움직여 값을 조정해주세요")
+                    .font(NMFont.pre_regular_16)
+            }
+            .padding(.horizontal, 20)
+            .padding(.bottom, 10)
+            
+            Slider(
+                value: $selectedNumber,
+                in: 1...17,
+                step: 1
+            )
+            .tint(.keyblue)
+            .padding(.horizontal, 52.5)
+            .padding(.top, 74)
+            .padding(.bottom, 28)
+            .onChange(of: selectedNumber) { oldValue, newValue in
+                selectedMoneyRange = .init(rawValue: Int(newValue))
+            }
+            
+            Text(selectedMoneyRange?.description ?? "")
+                .font(NMFont.pre_semibold_20)
+                .foregroundStyle(.text1)
+                .padding(.horizontal, 24)
+                .padding(.vertical, 12)
+                .background(
+                    RoundedRectangle(cornerRadius: 15)
+                        .foregroundStyle(Color.white1)
+                        .shadow(color: .black.opacity(0.1), radius: 2, x: 0, y: 0)
+                )
+                .frame(maxWidth: .infinity, alignment: .center)
+        }
     }
 }
 
