a69021a5e3909563e88f879a791a38a73285f3fb
SSOL <msseock@gmail.com>
2025-08-23 14:53:42 +0900

    🎉 create project

---
+++ NangManPa/NangManPa/NangManPaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NangManPaApp.swift
+//  NangManPa
+//
+//  Created by 석민솔 on 8/23/25.
+//
+
+import SwiftUI
+
+@main
+struct NangManPaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
